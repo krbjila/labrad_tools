efaecc27f2c50593bf33d88944b7263b626cc978
krbjila <krb.jila@gmail.com>
2019-10-03 20:03:46 -0600

    km cleaning unused servers

--- msquared_comb_lock/lock-q.py
+++
@@ -1,6 +0,0 @@
-from msquared_comb_lock import MSquaredCombLockServer
-
-if __name__ == '__main__':
-    from labrad import util
-    server = MSquaredCombLockServer('./config-q.json')
-    util.runServer(server)
