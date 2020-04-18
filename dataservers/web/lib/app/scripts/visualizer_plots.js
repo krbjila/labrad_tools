8b9dc99c54dfb0a11f8d0074e09806ac7e25f4af
Kyle Matsuda <kyle.matsuda@colorado.edu>
2020-04-17 19:28:12 -0600

    one axis plot done?

--- dataservers/web/lib/app/scripts/visualizer_plots.js
+++ dataservers/web/lib/app/scripts/visualizer_plots.js
@@ -2,6 +2,7 @@
 
   var typeLookup = [];
   var colorLookup = {};
+  var sequenceBoundaries = [];
 
   var sequence; var meta;
   var sequence_list;
@@ -18,7 +19,6 @@
     "#8a2be2"
   ]
   const analog_colors = d3.schemeCategory10;
-  console.log(analog_colors);
 
   var focusSet = false;
   var focusIndex;
@@ -56,7 +56,16 @@
       alert("Please put a number here!");
     }
     else {
-      $(this).val($val);
+      const maxpostime = sequence[TIMING_CHANNEL].slice(-1)[0][0] - x.domain()[0];
+      const maxnegtime = -1 * x.domain()[0];
+
+      if ($val*1e-3 > maxpostime) {
+        $val = 1e3 * maxpostime;
+      }
+      else if ($val*1e-3 < maxnegtime) {
+        $val = 1e3 * maxnegtime;
+      }
+      $(this).val($val.toFixed(3));
       updatePlot();
     }
   });
@@ -67,7 +76,12 @@
       alert("Please put a nonnegative number here!");
     }
     else {
-      $(this).val($val);
+      const maxtime = sequence[TIMING_CHANNEL].slice(-1)[0][0] - x.domain()[0];
+
+      if ($val*1e-3 > maxtime) {
+        $val = 1e3 * maxtime;
+      }
+      $(this).val($val.toFixed(3));
       updatePlot();
     }
   });
@@ -80,11 +94,11 @@
           let max = Math.max(-1.0*d3.min(d.data, x => x[1]), d3.max(d.data, x => x[1]));
           // Avoid divide by zero
           max = Math.max(1, max);
-          sequence_array[i].data = d.data.map(x => [x[0], x[1] / max]);
+          sequence_array[i].scaledData = d.data.map(x => [x[0], x[1] / max]);
         });
 
         y = d3.scaleLinear()
-          .domain([-1, 1])
+          .domain([-1.1, 1.1])
           .range([height - margins.top, margins.bottom]);
         yAxis = d3.axisLeft(y);
 
@@ -96,10 +110,11 @@
           if (typeLookup[d.nameloc] === "digital") {
             sequence_array[i].data = d.data.map(x => [x[0], (x[1] > 0) ? TTL_HIGH : 0]);
           }
+          sequence_array[i].scaledData = sequence_array[i].data;
         });
 
         y = d3.scaleLinear()
-          .domain([-10, 10])
+          .domain([-11, 11])
           .range([height - margins.top, margins.bottom]);
         yAxis = d3.axisLeft(y);
 
@@ -136,34 +151,15 @@
         sequence_array.push(
           {
             nameloc: $idn,
-            data: sequence[$idn]
+            name: $(this).attr('data-name'),
+            data: sequence[$idn],
+            scaledData: sequence[$idn]
           }
         );
       });
 
       updateAxes();
-
-      // // Trim data to domain
-      // const xdom = x.domain();
-      // const trimmedSequence = sequence_array
-      //   .map(function (d,i) {
-      //     const out = {
-      //       nameloc: d.nameloc,
-      //       data: d.data.slice(
-      //         d3.bisectLeft(d.data.map(d => d[0]), xdom[0], 1),
-      //         d3.bisectRight(d.data.map(d => d[0]), xdom[1], 1) + 1
-      //         )
-      //     };
-      //     return out;
-      //   });
-
-      const clip = svg.append("clipPath")
-        .attr("id", "clip-rect")
-        .append("rect")
-          .attr("x", margins.left)
-          .attr("y", margins.top)
-          .attr("width", width - margins.left - margins.right)
-          .attr("height", height - margins.top - margins.bottom);
+      drawSequenceBoundaries();
 
       const path = svg.select("g.lines")
         .selectAll("path")
@@ -171,11 +167,39 @@
           .join("path")
             .attr("fill", "none")
             .attr("clip-path", "url(#clip-rect)")
-            .attr("stroke-width", 2)
+            .attr("stroke-width", 3)
             .attr("stroke", d => colorLookup[d.nameloc])
-            .attr("d", d => line(d.data));
+            .attr("d", d => line(d.scaledData));
       svg.call(mouseActions, path);
     }
+  }
+
+  function drawSequenceBoundaries() {
+    sequenceBoundaries.length = 0;
+
+    var time = 0;
+    KRbVisualizer.sequences.forEach(function(d, i) {
+      sequenceBoundaries.push({
+        name: d.name,
+        time: time
+      });
+      time += d.duration;
+    });
+
+    svg.select("g.sequence-lines")
+      .selectAll("line")
+        .data(sequenceBoundaries)
+        .join("line")
+          .attr("fill", "none")
+          .attr("clip-path", "url(#clip-rect)")
+          .attr("stroke-width", 1)
+          .attr("stroke", "black")
+          .attr("stroke-dasharray", "1, 3")
+          .attr("stroke-linecap", "round")
+          .attr("x1", d => x(d.time))
+          .attr("x2", d => x(d.time))
+          .attr("y1", d => margins.top)
+          .attr("y2", d => height - margins.bottom);
   }
 
   function mouseActions(svg, path) {
@@ -184,23 +208,47 @@
       .on("mouseenter", entered)
       .on("mouseleave", left)
 
-    const dot = svg.append("g")
-      .attr("display", "none");
-    dot.append("circle")
-      .attr("r", 2.5);
-    dot.append("text")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("text-anchor", "middle")
-        .attr("y", -10);
+    const dot = svg.select("g.dot");
+    const sequenceTag = svg.select("g.sequence-tag");
+    const sequenceBackground = svg.select("g.sequence-background");
+
 
     function moved() {
       d3.event.preventDefault();
       const ym = y.invert(d3.event.layerY - margins.top);
       const xm = x.invert(d3.event.layerX - margins.left);
-      
+
+      if (xm >= x.domain()[1]) {
+        return;
+      }
+
+      // First handle sequence borders
+      const iseq1 = d3.bisectLeft(sequenceBoundaries.map(x => x.time), xm, 1);
+      const iseq0 = iseq1 - 1;
+
+      const time0 = Math.max(x.domain()[0], sequenceBoundaries[iseq0].time);
+      var time1;
+      if (iseq1 === sequenceBoundaries.length) {
+        time1 = x.domain()[1];
+      }
+      else {
+        time1 = Math.min(x.domain()[1], sequenceBoundaries[iseq1].time);
+      }
+      const anchor = 0.5 * (time0 + time1);
+
+      sequenceTag
+        .attr("transform", "translate(" + x(anchor) + "," + (height - margins.bottom) + ")")
+        .select("text")
+          .text(sequenceBoundaries[iseq0].name);
+
+      sequenceBackground.select("rect")
+        .attr("x", x(time0))
+        .attr("width", x(time1) - x(time0));
+      sequenceBackground.lower();
+
+      // Next handle lines
       if (focusSet) {
-        const channelData = sequence_array[focusIndex].data;
+        const channelData = sequence_array[focusIndex].scaledData;
         const i0 = d3.bisectLeft(channelData.map(x => x[0]), xm, 1);
         const i1 = i0 - 1;
         var inner = channelData[i0][0] - xm < xm - channelData[i1][0] ? i0 : i1;
@@ -211,15 +259,15 @@
         // If we have an ordered array ts = [t0, t1, t2, ...], bisectLeft(ts, t)
         // returns the index where we can insert t
         // However need to do several maps over this to get into the correct arrays
-        const i0s = sequence_array.map(d => d3.bisectLeft(d.data.map(x => x[0]), xm, 1));
+        const i0s = sequence_array.map(d => d3.bisectLeft(d.scaledData.map(x => x[0]), xm, 1));
         const i1s = i0s.map(d => d - 1);
 
         // Again looks gnarly. Just want to find whether xm is closer to i0s[i] or i1s[i]
         // for each i
         const is = sequence_array.map(
-          (d,i) => (d.data[i0s[i]][0] - xm < xm - d.data[i1s[i]][0]) ? i0s[i] : i1s[i]
+          (d,i) => (d.scaledData[i0s[i]][0] - xm < xm - d.scaledData[i1s[i]][0]) ? i0s[i] : i1s[i]
         );
-        const dys = sequence_array.map((d, i) => Math.abs(ym - d.data[is[i]][1]));
+        const dys = sequence_array.map((d, i) => Math.abs(ym - d.scaledData[is[i]][1]));
 
         focusIndex = 0;
         let temp = dys[0];
@@ -231,14 +279,18 @@
         }
         var inner = is[focusIndex];
       }
-      const val = sequence_array[focusIndex].data[inner];
+      const val = sequence_array[focusIndex].scaledData[inner];
+
+      dot.attr("transform", "translate("+ x(val[0]) + "," + y(val[1]) + ")")
+        .raise();
+      svg.select("#text-dot-name")
+        .text(sequence_array[focusIndex].name);
+      // Put the real voltage out
+      svg.select("#text-dot-voltage")
+        .text(sequence_array[focusIndex].data[inner][1].toFixed(3) + " V");
 
       path.attr("stroke", (d,i) => focusIndex === i ? colorLookup[d.nameloc] : "#ddd")
         .filter((d,i) => focusIndex === i).raise();
-
-      dot.attr("transform", "translate("+ x(val[0]) + "," + y(val[1]) + ")")
-      dot.select("text")
-        .text(sequence_array[focusIndex].nameloc + " = " + val[1].toFixed(3));
     }
     function clicked() {
       if (margins.top < d3.event.layerY
@@ -248,7 +300,9 @@
         focusSet = !focusSet;
     }
     function entered() {
-      dot.attr("display", null);  
+      dot.attr("display", null);
+      sequenceTag.attr("display", null);
+      sequenceBackground.attr("display", null);
 
       if (!focusSet) {
         path.attr("stroke", "#ddd");
@@ -256,6 +310,8 @@
     }
     function left() {
       dot.attr("display", "none");
+      sequenceTag.attr("display", "none");
+      sequenceBackground.attr("display", "none");
 
       if (!focusSet){
         path.attr("stroke", d => colorLookup[d.nameloc]);
@@ -336,7 +392,54 @@
       .call(yAxis);
 
     svg.append("g")
-      .attr("class", "lines")
+      .attr("class", "lines");
+
+    svg.append("g")
+      .attr("class", "sequence-lines");
+
+    svg.append("clipPath")
+      .attr("id", "clip-rect")
+      .append("rect")
+        .attr("x", margins.left)
+        .attr("y", margins.top)
+        .attr("width", width - margins.left - margins.right)
+        .attr("height", height - margins.top - margins.bottom);
+
+    const dot = svg.append("g")
+      .attr("class", "dot")
+      .attr("display", "none")
+    dot.append("circle")
+      .attr("r", 2.5)
+    dot.append("text")
+      .attr("id", "text-dot-name")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("text-anchor", "middle")
+      .attr("y", -10);
+    dot.append("text")
+      .attr("id", "text-dot-voltage")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("text-anchor", "middle")
+      .attr("y", 15);
+
+    svg.append("g")
+      .attr("class", "sequence-tag")
+      .attr("display", "none")
+      .append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 12)
+        .attr("text-anchor", "middle")
+        .attr("y", -10);
+
+    svg.append("g")
+      .attr("class", "sequence-background")
+      .attr("display", "none")
+      .append("rect")
+        .attr("y", margins.top)
+        .attr("height", height - margins.top - margins.bottom)
+        .attr("fill", "#eee")
+        .attr("stroke", "none");
   }
 
 })();
