89be526f2617a07850dfa20d97a767a0364ef3cc
krbjila <krb.jila@gmail.com>
2020-08-20 19:12:44 -0600

    Included field correction factor

--- electrode/calibrations.py
+++ electrode/calibrations.py
@@ -1,11 +1,11 @@
 # E Field Calibration
 EFC = {
-	'LP' : {'m' : -2001.63, 'b': 0.012},
-	'UP' : {'m' : -2001.75, 'b': 2.245},
-	'LW' : {'m' : -2001.97, 'b': 3.912},
-	'LE' : {'m' : -1999.95, 'b': -4.048},
-	'UW' : {'m' : -2001.31, 'b': -2.304},
-	'UE' : {'m' : -2001.95, 'b': 1.793}
+	'LP' : {'m' : -2001.16, 'b': -0.336},
+	'UP' : {'m' : -2001.09, 'b': -0.309},
+	'LW' : {'m' : -2001.11, 'b': -0.349},
+	'LE' : {'m' : -1999.76, 'b': -0.221},
+	'UW' : {'m' : -2001.10, 'b': -0.315},
+	'UE' : {'m' : -2001.07, 'b': -0.323}
 }
 
 ZEROS = {x : -EFC[x]['b']/EFC[x]['m'] for x in EFC.keys()}
@@ -14,7 +14,7 @@ ZEROS = {x : -EFC[x]['b']/EFC[x]['m'] for x in EFC.keys()}
 PLATE_SEPARATION = 0.6 # cm
 
 # Fudge factor away from parallel plate capacitor
-RODS_CORRECTION = 0.96079
+RODS_CORRECTION = 0.96079*1.01315
 
 # Safety limit for DACs; HV is limited to 2000x this value
 DAC_LIMIT = 2.5
