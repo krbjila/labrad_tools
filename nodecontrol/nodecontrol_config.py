9cb79bab0bb6123b8965e7c1ccaf090cd86febd5
krbjila <krb.jila@gmail.com>
2020-11-19 15:07:48 -0700

    changes to nodestart

--- nodecontrol/nodecontrol_config.py
+++ nodecontrol/nodecontrol_config.py
@@ -1,14 +1,28 @@
-node_dict = {
-    'node krbjila': [
-	# start hardware interfaces first
-        'gpib', 
-       	'okfpga',
-        'arduino',
-#        # electrode gui
-        'electrode',
-	# start device servers next
-        'sequencer',
-	# start conductor last
-        'conductor', 
-    ],
-}
+node_dicts = [
+    {
+        'node wavemeterlaptop' : [
+            'wavemeter'
+        ]
+    },
+    {
+        'node imaging' : [
+            'ad9910',
+            'okfpga',
+            'logging'
+        ]
+    },
+    {
+        'node krbjila': [
+            # start hardware interfaces first
+            'gpib', 
+            'okfpga',
+            'arduino',
+#            # electrode gui
+            'electrode',
+            # start device servers next
+            'sequencer',
+            # start conductor last
+            'conductor', 
+        ]
+    },
+]
