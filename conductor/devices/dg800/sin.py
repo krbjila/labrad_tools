7c3a6b08e0d50e3c17c8791f384b10de58347b08
KRbJILA <krb.jila@gmail.com>
2020-12-03 10:21:52 -0700

    changed name of dg800 server in conductor device

--- conductor/devices/dg800/sin.py
+++ conductor/devices/dg800/sin.py
@@ -20,15 +20,15 @@ class Sin(ConductorParameter):
     @inlineCallbacks
     def initialize(self):
         self.cxn = yield connectAsync()
-        device = yield self.cxn.polarkrb_dg800.get_devices()[0]
-        yield self.cxn.polarkrb_dg800.select_device(device)
+        device = yield self.cxn.imaging_dg800.get_devices()[0]
+        yield self.cxn.imaging_dg800.select_device(device)
         try:
-          yield self.cxn.polarkrb_dg800.set_sin(1,
+          yield self.cxn.imaging_dg800.set_sin(1,
            self.value['freq1'], self.value['amplitude1'], self.value['offset1'], self.value['phase1'])
-          yield self.cxn.polarkrb_dg800.set_sin(2,
+          yield self.cxn.imaging_dg800.set_sin(2,
            self.value['freq2'], self.value['amplitude2'], self.value['offset2'], self.value['phase2'])
-          yield self.cxn.polarkrb_dg800.set_output(1,self.value['outputl'])
-          yield self.cxn.polarkrb_dg800.set_output(2,self.value['output2'])
+          yield self.cxn.imaging_dg800.set_output(1,self.value['outputl'])
+          yield self.cxn.imaging_dg800.set_output(2,self.value['output2'])
         except Exception as e:
           print(e)
 
@@ -36,11 +36,11 @@ class Sin(ConductorParameter):
     def update(self):
         if self.value:
             try:
-        	      yield self.cxn.polarkrb_dg800.set_sin(1,
+        	      yield self.cxn.imaging_dg800.set_sin(1,
                  self.value['freq1'], self.value['amplitude1'], self.value['offset1'], self.value['phase1'])
-                yield self.cxn.polarkrb_dg800.set_sin(2,
+                yield self.cxn.imaging_dg800.set_sin(2,
                  self.value['freq2'], self.value['amplitude2'], self.value['offset2'], self.value['phase2'])
-                yield self.cxn.polarkrb_dg800.set_output(1,self.value['outputl'])
-                yield self.cxn.polarkrb_dg800.set_output(2,self.value['output2'])
+                yield self.cxn.imaging_dg800.set_output(1,self.value['outputl'])
+                yield self.cxn.imaging_dg800.set_output(2,self.value['output2'])
             except Exception as e:
                 print(e)
