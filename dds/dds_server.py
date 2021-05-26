bec24e59de4432f3784971fc8e21dab0266ab6a9
KRb <krb.jila@gmail.com>
2021-05-26 16:00:10 -0600

    adding dds server from krbg2 computer

---
+++ dds/dds_server.py
@@ -0,0 +1,160 @@
+"""
+### BEGIN NODE INFO
+[info]
+name = dds
+version = 1.1
+description = 
+instancename = %LABRADNODE%_dds
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+import sys
+
+from time import sleep
+
+from labrad.server import LabradServer, setting, Signal
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+import json
+
+import datetime
+import os
+
+from copy import deepcopy
+
+sys.path.append('../')
+from server_tools.device_server import DeviceServer
+
+class ddsServer(DeviceServer):
+    """Provides access to hardware's serial interface """
+    name = '%LABRADNODE%_dds'
+    current_values = {}
+    last_update_time = ''
+
+    @setting(10)
+    def get_channels(self, cntx):
+        channels = {c.name: c.__dict__
+            for d in self.devices.values() 
+                for c in d.channels}
+        return json.dumps(channels, default=lambda x: None)
+
+    # Convenient way to get the channel output from iPython, etc.
+    @setting(11, dev='s', board='i', channel='i', returns='s')
+    def getChannel(self, c, dev, board, channel):
+    	
+    	# Try to open the device
+        try:
+            device = self.devices[dev]
+        except:
+            return "Can't find device.\n"
+
+    	# Try to get the channel
+        try:
+            ret = device.getChannel(board, channel)
+        except:
+            return "Can't find channel.\n"
+
+    	# Try to get the current value
+        if ret != None:
+    	   return "{} at {} MHz.\n".format(ret['name'], ret['frequency'])
+    	else:
+    		return "Requested channel is not initialized.\n"
+
+
+    # Updating the DDS
+    @setting(12, sequence='s')
+    def update_dds(self, c, sequence):
+        # Get the sequence
+        sequence = json.loads(sequence)
+
+        # Check whether any channels are actually changing!    
+        fixed_sequence = {}
+        for key, value in sequence.items():
+
+            # If the device name ("key") is not in the self.current_values dict and
+            # it refers to a real device, add it
+            if key not in self.current_values and key in self.devices:
+                self.current_values[key] = {}
+
+            # If the key is valid
+            if key in self.devices:
+                fixed_sequence[key] = []
+                channels = [c.__dict__ for c in self.devices[key].channels]
+                
+                # Data is coming in as
+                # sequence = {device: [{"address": address, "frequency": frequency}, ...], ...}
+                for entry in value:
+                    # For convenience, the current values are located in a dict of format:
+                    # self.current_values = {
+                    #     device: {"channel_name": frequency}
+                    # }
+                    # So we need to get the channel names
+                    for c in channels:
+                        if entry['address'] == c['loc']:
+                            entry['name'] = c['name']
+                    try:
+                        # Check if the channel name is in the dict already
+                        if entry['name'] in self.current_values[key].keys():
+                            # If frequency is not the same, then we add it to the list of changes
+                            if entry['frequency'] != self.current_values[key][entry['name']]:
+                                fixed_sequence[key].append(entry)
+                        # If it's not, that's because the channel has never been updated since the server has been alive
+                        # so it doesn't know what the value is
+                        else:
+                            fixed_sequence[key].append(entry)
+
+                        # Set the current value in the dict to the current frequency
+                        self.current_values[key][entry['name']] = entry['frequency']
+                    except Exception as e:
+                        print e
+
+
+        # Expect a dict of format:
+        # {"device": [{"address": address, "frequency": frequency}, ... ], ...}
+        flag = 0 # Flag keeps track of whether any channels were actually updated
+        for key, value in fixed_sequence.items():
+            if key in self.devices and len(value) > 0:
+                dev = self.devices[key]
+                dev.program_frequencies(value)
+                flag = 1
+
+        # If an update occurred, need to update the backup
+        if flag == 1:
+            # Check directory for backup
+            nowtime = datetime.datetime.now()
+            # savedir = nowtime.strftime('.\\backup\\' + '%Y%m%d\\')
+            savedir = nowtime.strftime('.\\backup\\')
+            if not os.path.isdir(savedir):
+                os.makedirs(savedir)
+
+            self.last_update_time = nowtime.strftime('%H:%M:%S')
+
+            # # Write the json string to the backup file
+            # with open(savedir + nowtime.strftime('%H_%M_%S') + ".txt", 'w') as f:
+            #     f.write(json.dumps(fixed_sequence))
+            
+            # Write the json string to the backup file
+            with open(savedir + nowtime.strftime('%Y%m%d') + ".txt", 'a') as f:
+                f.write(self.last_update_time + " :\n")
+                f.write(json.dumps(fixed_sequence))
+                f.write("\n")
+
+
+    @setting(13, returns='s')
+    def get_last_update_time(self, c):
+        return self.last_update_time
+
+
+__server__ = ddsServer()
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(__server__)
+
---
+++ dds/devices/ad9959xem6001.py
@@ -0,0 +1,139 @@
+import json
+from twisted.internet.defer import inlineCallbacks, returnValue
+import sys
+
+sys.path.append('../../')
+from server_tools.device_server import DeviceWrapper
+
+from time import sleep
+
+### DDS parameters ####
+
+MULTIPLIER = 20
+VCOGAIN = 1 # 1 for on, 0 for off
+CLOCK = 20
+
+class ddsChannel(object):
+    def __init__(self, config):
+        """ defaults """
+        self.channel_type = 'dds'
+
+        self.loc = config['loc']
+        self.name = config['name']
+
+        self.frequency = 0
+
+        self.key = self.name + '@{},{}'.format(self.loc[0], self.loc[1])
+
+        """ non-defaults """
+        for key, value in config.items():
+            setattr(self, key, value)
+
+class ad9959xem6001(DeviceWrapper):
+    # DDS Parameters
+    clk = CLOCK
+    multiplier = MULTIPLIER
+    vcogain = VCOGAIN
+    bits = 32
+    numchannels = 12
+
+    def __init__(self, config):
+        """ defaults """
+        self.update_parameters = []
+        self.init_commands = []
+
+        self.bitfile = 'dds3_ad9959.bit'
+        self.data_wires = [0x00, 0x01, 0x02]
+
+        self.current_values = [None]*self.numchannels
+
+        """ non-defaults"""
+        for key, value in config.items():
+            setattr(self, key, value)
+
+        channel_wrappers = [None]*self.numchannels
+        for c in self.channels:
+            c['board_name'] = self.name
+            wrapper = ddsChannel(c)
+            board, channel = wrapper.loc
+            channel_wrappers[4*board + channel] = wrapper
+        self.channels = channel_wrappers
+
+        for c in self.channels:
+            c.board = self
+      
+        super(ad9959xem6001, self).__init__({})
+
+    @inlineCallbacks
+    def initialize(self):
+        yield self.connection.program_bitfile(self.bitfile)
+
+    def getValue(self, board, channel):
+        return (self.current_values[4*board + channel])
+
+    ###########################################################
+    ############ Hardware-dependent implementation ############
+    #### See ~/Desktop/Luigi/DDS/KRb3DDSAD9959 for details ####
+    ###########################################################
+
+    @inlineCallbacks
+    # sequence should be a list of dicts
+    # [{"address": address, "frequency": frequency}, ..., ]
+    def program_frequencies(self, sequence):
+        for item in sequence:
+            try:
+                address = item['address']
+                frequency = item['frequency']
+
+                if frequency >= 0 and frequency < self.clk * self.multiplier / 2.0:
+                    # Calculate tuning word 
+                    tuningword = self.freq2word(float(frequency), self.clk * self.multiplier)
+                    ep02, ep01 = self.wordsplit(tuningword)
+
+                    # Get channel select and clock 
+                    ep00 = self.cselect(address[0], address[1], self.multiplier, self.vcogain)
+
+                    # Get channel name
+                    name = self.channels[4*address[0] + address[1]].name
+                    print("{0} MHz written to {1}".format(frequency, name))
+
+                    # Update current_values array
+                    self.current_values[4*address[0] + address[1]] = {"name": name, "frequency": frequency}
+
+                    # Set wire ins to FPGA
+                    yield self.connection.set_wire_in(0x00, ep00)
+                    yield self.connection.set_wire_in(0x01, ep01)
+                    yield self.connection.set_wire_in(0x02, ep02)
+                    yield self.connection.update_wire_ins()
+
+                    sleep(0.1)
+                    # yield sleep(0.001)
+
+                    yield self.connection.set_wire_in(0x00, 0)
+                    yield self.connection.update_wire_ins()
+                else:
+                    print "Channel ({}, {}): Frequency out of range.\n".format(address[0], address[1])
+            
+            except Exception as e:
+                print e
+
+    def freq2word(self, frequency, clock):
+        #Convert frequency in MHz to tuning word
+        resolution = 2**self.bits
+        tuningword = int(frequency*resolution/clock)
+        return "{0:032b}".format(tuningword)
+
+    def wordsplit(self, word):
+        #Split tuning word for passing to fpga
+        return int(word[0:16],2), int(word[16:32],2)
+
+    def cselect(self, dds, channel, multiplier, vcogain):
+        #Generate words for channel selection and clock configuration
+        x1 = ["0"]*8
+        x1[3 - channel] = "1"
+        x2 = "{0:05b}".format(multiplier)
+        x3 = str(1*vcogain)
+        x4 = "{0:02b}".format(dds + 1)
+        x = "".join(x1) + x2 + x3 + x4
+        return int(x,2)
+
