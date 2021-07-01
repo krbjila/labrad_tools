121a45ce91e501057fe473c90f3b22ce8a46f276
krbjila <krb.jila@gmail.com>
2021-07-01 12:09:17 -0600

    comment test sequencer

--- sequencer/test_sequencer.py
+++ sequencer/test_sequencer.py
@@ -1,5 +1,12 @@
+'''
+Run me with okfpga/test/test_server.py to run the Sequencer server 
+without connections to FPGAs.
+
+The FPGA connections are mocked up in the TestOkfpga class
+(all methods just pass)
+'''
 from sequencer import SequencerServer
-    
+
 if __name__ == "__main__":
     from labrad import util
     util.runServer(SequencerServer('./test_config.json'))
