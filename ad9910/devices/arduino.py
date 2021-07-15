035046f3148b7656da806d05af2d0b12f8a748d3
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2021-07-15 14:54:56 -0600

    fix tabs

--- ad9910/devices/arduino.py
+++ ad9910/devices/arduino.py
@@ -8,11 +8,11 @@ from server_tools.device_server import DeviceWrapper
 from time import sleep
 
 tx_types_dictionary = {
-	'single': 0,
-	'drLimits': 1,
-	'drStepSize': 2,
-	'drRate': 3,
-	'sweepInvert': 4 # send 0 for no invert, 1 for invert
+    'single': 0,
+    'drLimits': 1,
+    'drStepSize': 2,
+    'drRate': 3,
+    'sweepInvert': 4 # send 0 for no invert, 1 for invert
 }
 
 SYSCLK = 1000 # MHz
@@ -22,28 +22,28 @@ SYSCLK = 1000 # MHz
 #
 # Argument: frequency in MHz
 def calc_ftw(freq):
-	calc_ftw = int(2**(32) * float(freq) / SYSCLK)
-	# Format string MSB first
-	hex_str = '{:08X}'.format(calc_ftw)
-	res = ""
-	for i in range(0, 4):
-		res = res + "0x" + hex_str[2*i:(2*i+2)] + ","
-	return res
+    calc_ftw = int(2**(32) * float(freq) / SYSCLK)
+    # Format string MSB first
+    hex_str = '{:08X}'.format(calc_ftw)
+    res = ""
+    for i in range(0, 4):
+        res = res + "0x" + hex_str[2*i:(2*i+2)] + ","
+    return res
 
 # Returns string of bytes MSB
 # in format "byte_1,byte_0,"
 #
 # Argument: phase in degrees
 def calc_pow(phase):
-	phase = phase % 360
-	calc_pow = int(2**(16) * float(phase) / 360)
+    phase = phase % 360
+    calc_pow = int(2**(16) * float(phase) / 360)
 
-	# Format string
-	hex_str = '{0:04X}'.format(calc_pow)
-	res = ""
-	for i in range(0,2):
-		res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
-	return res
+    # Format string
+    hex_str = '{0:04X}'.format(calc_pow)
+    res = ""
+    for i in range(0,2):
+        res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
+    return res
 
 # Returns string of bytes MSB
 # in format "byte_1,byte_0,"
@@ -51,84 +51,84 @@ def calc_pow(phase):
 # Argument: relative amplitude in dB relative to full scale
 # Range is 0 to -80 dB
 def calc_ampl(ampl):
-	# Upper limit is 0 dB
-	if ampl >= 0:
-		return "0x3F,0xFF,"
-	# Lower limit is -80 dB
-	elif ampl < -80:
-		ampl = -80
+    # Upper limit is 0 dB
+    if ampl >= 0:
+        return "0x3F,0xFF,"
+    # Lower limit is -80 dB
+    elif ampl < -80:
+        ampl = -80
 
-	AMPL = int(2**(14) * pow(10, float(ampl)/20))
-	hex_str = '{0:04X}'.format(AMPL)
-	res = ""
-	for i in range(0,2):
-		res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
-	return res
+    AMPL = int(2**(14) * pow(10, float(ampl)/20))
+    hex_str = '{0:04X}'.format(AMPL)
+    res = ""
+    for i in range(0,2):
+        res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
+    return res
 
 # Returns string of bytes MSB
 # in format "byte_1,byte_0,"
 #
 # Argument: step interval in microseconds
 def calc_step_interval(interval):
-	min_interval = float(4) / SYSCLK
-	max_interval = (2**(16) - 1) * min_interval
-	if (interval < min_interval):
-		interval = min_interval
-	elif (interval > max_interval):
-		interval = max_interval
+    min_interval = float(4) / SYSCLK
+    max_interval = (2**(16) - 1) * min_interval
+    if (interval < min_interval):
+        interval = min_interval
+    elif (interval > max_interval):
+        interval = max_interval
 
-	intW = int(interval / min_interval)
-	# Format string
-	hex_str = '{0:04X}'.format(intW)
-	res = ""
-	for i in range(0,2):
-		res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
-	return res
+    intW = int(interval / min_interval)
+    # Format string
+    hex_str = '{0:04X}'.format(intW)
+    res = ""
+    for i in range(0,2):
+        res = res + "0x" + hex_str[2*i:(2*i + 2)] + ","
+    return res
 
 # Calculates ramp parameters
 # Input:
-# 	start: start frequency in MHz
-#	stop: stop frequency in MHz
-#	dt: duration in milliseconds
-#	nsteps (optional): number of steps in sweep, default=1000
+#     start: start frequency in MHz
+#    stop: stop frequency in MHz
+#    dt: duration in milliseconds
+#    nsteps (optional): number of steps in sweep, default=1000
 #
 # Returns: Dictionary with items:
-#	upper: upper limit frequency in MHz
-#	lower: lower limit frequency in MHz
-#	slope: slope polarity (+1 for positive ramp, -1 for negative ramp)
-#	interval: step interval for accumulator in microseconds
-#	step: frequency change per step in MHz
+#    upper: upper limit frequency in MHz
+#    lower: lower limit frequency in MHz
+#    slope: slope polarity (+1 for positive ramp, -1 for negative ramp)
+#    interval: step interval for accumulator in microseconds
+#    step: frequency change per step in MHz
 def calc_ramp_parameters(start, stop, dt, nsteps=1000):
-	slope = 0
-	if start <= stop:
-		slope = 1
-		lower = start
-		upper = stop
-	else:
-		slope = -1
-		lower = stop
-		upper = start
+    slope = 0
+    if start <= stop:
+        slope = 1
+        lower = start
+        upper = stop
+    else:
+        slope = -1
+        lower = stop
+        upper = start
 
-	# dt is given in ms
-	# Convert to microseconds
-	# To get dt per step, divide by nsteps
-	# This is step interval in microseconds
-	dt_each = 1000 * float(dt) / nsteps
-	df_each = float(upper - lower) / nsteps
+    # dt is given in ms
+    # Convert to microseconds
+    # To get dt per step, divide by nsteps
+    # This is step interval in microseconds
+    dt_each = 1000 * float(dt) / nsteps
+    df_each = float(upper - lower) / nsteps
 
-	# DRG automatically starts from lower limit
-	# If slope is negative, need to do something fancy
-	if slope == 1:
-		p_interval = dt_each
-		n_interval = 0 # minimum step interval
-		p_step = df_each
-		n_step = df_each
-	elif slope == -1:
-		p_interval = 0 # minimum step interval; quickly sweep up to upper limit
-		n_interval = dt_each
-		p_step = float(df_each) * nsteps # positive step size is the whole sweep interval
-		n_step = df_each
-	return {'upper': upper, 'lower': lower, 'slope': slope, 'p_interval': p_interval, 'n_interval': n_interval, 'p_step': p_step, 'n_step': n_step}
+    # DRG automatically starts from lower limit
+    # If slope is negative, need to do something fancy
+    if slope == 1:
+        p_interval = dt_each
+        n_interval = 0 # minimum step interval
+        p_step = df_each
+        n_step = df_each
+    elif slope == -1:
+        p_interval = 0 # minimum step interval; quickly sweep up to upper limit
+        n_interval = dt_each
+        p_step = float(df_each) * nsteps # positive step size is the whole sweep interval
+        n_step = df_each
+    return {'upper': upper, 'lower': lower, 'slope': slope, 'p_interval': p_interval, 'n_interval': n_interval, 'p_step': p_step, 'n_step': n_step}
 
 
 def create_program_string(line, data_type, byte_string):
