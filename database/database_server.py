d193c0ee7da381f698e0a0a910d1d8d7250c8e77
KRb JILA <krb.jila@gmail.com>
2021-10-06 09:40:34 -0600

    fixes, added find_one

--- database/database_server.py
+++ database/database_server.py
@@ -28,9 +28,7 @@ class DatabaseServer(LabradServer):
     """
     Server for communicating with MongoDB databases.
 
-    Currently includes methods for inserting, modifying, and deleting data, but does not yet include query methods.
-
-    #TODO: Add query methods!
+    Currently includes methods for inserting, modifying, and deleting data, and the basic query method :meth:`find_one`. 
 
     Uses :py:mod:`bson.json_util` for serializing and deserializing `BSON <https://bsonspec.org/>`__ data.
     """
@@ -51,11 +49,11 @@ class DatabaseServer(LabradServer):
         self.close(c)
 
     @staticmethod
-    def InsertOneResultToDict(res):
+    def InsertOneResultToDict(result):
         """
         InsertOneResultToDict(res)
 
-        Converts a PyMongo :py:class:`pymongo.results.InsertOneResult` to a dictionary.
+        Converts a :py:class:`pymongo.results.InsertOneResult` to a dictionary.
 
         Args:
             res (:py:class:`pymongo.results.InsertOneResult`): The result to convert to a dictionary
@@ -70,7 +68,7 @@ class DatabaseServer(LabradServer):
         return return_dict
 
     @staticmethod
-    def InsertManyResultToDict(res):
+    def InsertManyResultToDict(result):
         """
         InsertManyResultToDict(res)
 
@@ -89,11 +87,11 @@ class DatabaseServer(LabradServer):
         return return_dict
 
     @staticmethod
-    def UpdateResultToDict(res):
+    def UpdateResultToDict(result):
         """
         UpdateResultToDict(res)
 
-        Converts a PyMongo :py:class:`pymongo.results.UpdateResult` to a dictionary.
+        Converts a :py:class:`pymongo.results.UpdateResult` to a dictionary.
 
         Args:
             res (:py:class:`pymongo.results.UpdateResult`): The result to convert to a dictionary
@@ -111,11 +109,11 @@ class DatabaseServer(LabradServer):
         return return_dict
 
     @staticmethod
-    def DeleteResultToDict(res):
+    def DeleteResultToDict(result):
         """
         DeleteResultToDict(res)
 
-        Converts a PyMongo :py:class:`pymongo.results.DeleteResult` to a dictionary.
+        Converts a :py:class:`pymongo.results.DeleteResult` to a dictionary.
 
         Args:
             res (:py:class:`pymongo.results.DeleteResult`): The result to convert to a dictionary
@@ -131,16 +129,16 @@ class DatabaseServer(LabradServer):
         return return_dict
 
     @inlineCallbacks
-    @setting(9, server='s', port='s', user='s', password='s', database='s', collection='s', timeout='i', returns='s')
-    def connect(self, c, server=None, port=None, user=None, password=None, database=None, collection=None, timeout=2000):
+    @setting(9, address='s', port='s', user='s', password='s', database='s', collection='s', timeout='i', returns='s')
+    def connect(self, c, address=None, port=None, user=None, password=None, database=None, collection=None, timeout=2000):
         """
-        connect(self, c, server=None, port=None, user=None, password=None, database=None, collection=None)
+        connect(self, c, address=None, port=None, user=None, password=None, database=None, collection=None)
 
         Connect to a MongoDB database. Connections are maintained per LabRAD context.
 
         Args:
             c: LabRAD context
-            server (str, optional): The server to connect to. Defaults to ``None``, in which case the server is loaded from ``mongodb.json`` if the file exists.
+            address (str, optional): The address to connect to. Defaults to ``None``, in which case the address is loaded from ``mongodb.json`` if the file exists.
             port (str, optional): The port to connect to. Defaults to ``None``, in which case the port is loaded from ``mongodb.json`` if the file exists. If the port is not specified in the file, the default port is ``27017``.
             user (str, optional): The user to connect as. Defaults to ``None``, in which case the user is loaded from ``mongodb.json`` if the file exists.
             password (str, optional): The user's password. Defaults to ``None``, in which case the password is loaded from ``mongodb.json`` if the file exists.
@@ -163,11 +161,11 @@ class DatabaseServer(LabradServer):
             print("Could not load MongoDB config: {}".format(e))
             config = {}
 
-        if server is None:
-            if "server" in config:
-                server = config["server"]
+        if address is None:
+            if "address" in config:
+                address = config["address"]
             else:
-                print("Could not connect to MongoDB database. No server specified.")
+                print("Could not connect to MongoDB database. No address specified.")
                 returnValue("{}")
         if port is None:
             if "port" in config:
@@ -187,11 +185,11 @@ class DatabaseServer(LabradServer):
                 print("Could not connect to MongoDB database. No password specified.")
                 returnValue("{}")
 
-        url = "mongodb://{}:{}@{}:{}/authSource=admin".format(user, password, address, port)
+        url = "mongodb://{}:{}@{}:{}/?authSource=admin".format(user, password, address, port)
         c.c = yield MongoClient(url)
         try:
             server_info = yield c.c.server_info()
-            server_info_str = bson.dumps(server_info)
+            server_info_str = dumps(server_info)
             
             if database is not None:
                 yield self.set_database(c, database)
@@ -232,7 +230,7 @@ class DatabaseServer(LabradServer):
 
         Args:
             c: LabRAD context
-            database (str): [description]
+            database (str): The name of the database to use
 
         Returns:
             bool: ``True`` if the database exists, ``False`` otherwise
@@ -258,7 +256,7 @@ class DatabaseServer(LabradServer):
 
         Args:
             c: LabRAD context
-            collection (str): [description]
+            collection (str): The name of the collection to use
 
         Returns:
             bool: ``True`` if the collection exists in the current database, ``False`` otherwise
@@ -275,7 +273,7 @@ class DatabaseServer(LabradServer):
             returnValue(False)
 
     @inlineCallbacks
-    @setting(13, document='s', returns=str)
+    @setting(13, document='s', returns='s')
     def insert_one(self, c, document):
         """
         insert_one(self, c, document)
@@ -294,13 +292,12 @@ class DatabaseServer(LabradServer):
         document = loads(document)
         try:
             result = yield c.c[c.database][c.collection].insert_one(document)
-            
-            returnValue(dumps(return_dict))
+            returnValue(dumps(DatabaseServer.InsertOneResultToDict(result)))
         except Exception as e:
             print("Could not insert document into collection {} of database {}: {}".format(c.collection, c.database, e))
             returnValue("{}")
 
-    @setting(14, documents='*s', ordered='b', returns=str)
+    @setting(14, documents='*s', ordered='b', returns='s')
     def insert_many(self, c, documents, ordered=True):
         """
         insert_many(self, c, documents, ordered=True)
@@ -319,13 +316,13 @@ class DatabaseServer(LabradServer):
         """
         documents = [loads(document) for document in documents]
         try:
-            result = yield c.c[c.database][c.collection].insert_many(document, ordered=ordered)
+            result = yield c.c[c.database][c.collection].insert_many(documents, ordered=ordered)
             returnValue(dumps(DatabaseServer.InsertManyResultToDict(result)))
         except Exception as e:
             print("Could not insert documents into collection {} of database {}: {}".format(c.collection, c.database, e))
             returnValue("{}")
 
-    @setting(15, db_filter='s', replacement='s', upsert='b', returns=str)
+    @setting(15, db_filter='s', replacement='s', upsert='b', returns='s')
     def replace_one(self, c, db_filter, replacement, upsert=False):
         """
         replace_one(self, c, db_filter, replacement, upsert=False)
@@ -352,7 +349,7 @@ class DatabaseServer(LabradServer):
             print("Could not replace document matching filter {} in collection {} of database {}: {}".format(db_filter, c.collection, c.database, e))
             returnValue("{}")
 
-    @setting(16, db_filter='s', update='s', upsert='b', returns=str)
+    @setting(16, db_filter='s', update='s', upsert='b', returns='s')
     def update_one(self, c, db_filter, update, upsert=False):
         """
         update_one(self, c, db_filter, update, upsert=False)
@@ -379,7 +376,7 @@ class DatabaseServer(LabradServer):
             print("Could not update document matching filter {} in collection {} of database {}: {}".format(db_filter, c.collection, c.database, e))
             returnValue("{}")
 
-    @setting(17, db_filter='s', update='s', upsert='b')
+    @setting(17, db_filter='s', update='s', upsert='b', returns='s')
     def update_many(self, c, db_filter, update, upsert=False):
         """
         update_many(self, c, db_filter, update, upsert=False)
@@ -409,7 +406,7 @@ class DatabaseServer(LabradServer):
     @setting(18, db_filter='s', returns='s')
     def delete_one(self, c, db_filter):
         """
-        delete_one(self, c, db_filter, update, upsert=False)
+        delete_one(self, c, db_filter)
 
         Delete a single document matching ``db_filter``.
         
@@ -422,7 +419,7 @@ class DatabaseServer(LabradServer):
         Returns:
             str: A BSON-dumped string of the :py:class:`pymongo.results.DeleteResult`
         """
-        update = loads(update)
+        db_filter = loads(db_filter)
         try:
             result = yield c.c[c.database][c.collection].delete_one(db_filter)
             returnValue(dumps(DatabaseServer.DeleteResultToDict(result)))
@@ -446,12 +443,40 @@ class DatabaseServer(LabradServer):
         Returns:
             str: A BSON-dumped string of the :py:class:`pymongo.results.DeleteResult`
         """
-        update = loads(update)
+        db_filter = loads(db_filter)
         try:
             result = yield c.c[c.database][c.collection].delete_many(db_filter)
             returnValue(dumps(DatabaseServer.DeleteResultToDict(result)))
         except Exception as e:
             print("Could not delete documents matching filter {} in collection {} of database {}: {}".format(db_filter, c.collection, c.database, e))
+            returnValue("{}")
+
+    @setting(20, db_filter='s', returns='s', projection='*s')
+    def find_one(self, c, db_filter, projection=None):
+        """
+        find_one(self, c, db_filter, projection=None)
+
+        Get a single document matching ``db_filter``.
+        
+        See :py:meth:`pymongo.collection.Collection.find_one` for information on how ``db_filter`` (called ``filter`` in the documentation) works.
+
+        Args:
+            c: LabRAD context
+            db_filter (str): BSON-dumped string of the filter
+            projection ([str]): a list of field names that should be returned in the result set. Defaults to ``None``, in which case all fields are returned.
+
+        Returns:
+            str: A BSON-dumped string of the document or ``{}`` if no document is found
+        """
+        db_filter = loads(db_filter)
+        try:
+            result = yield c.c[c.database][c.collection].find_one(db_filter, projection=projection)
+            if result is not None:
+                returnValue(dumps(result))
+            else:
+                returnValue("{}")
+        except Exception as e:
+            print("Could not find document matching filter {} in collection {} of database {}: {}".format(db_filter, c.collection, c.database, e))
             returnValue("{}")
 
 if __name__ == '__main__':
