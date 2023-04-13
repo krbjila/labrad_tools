c5d544dbbb917dfd6c8f74009449827a9fc75f00
Cal Miller <cami6171@colorado.edu>
2023-04-13 11:40:30 -0600

    fix compilation of areapulses with areapulse windows, add flexibility to give class instead of instance for pulses in sequences

--- synthesizer/tests/test_plots.py
+++ synthesizer/tests/test_plots.py
@@ -7,7 +7,12 @@ sys.path.append(parent)
 import synthesizer_sequences as ss
 
 seq = {
-    0: [ss.SetTransition(ss.Transition(2E6, [0.5], [1000])), ss.CORPSE(pi/2, phase=pi/2)],
+    0: [
+        ss.SetTransition(ss.Transition(2E6, [0.5], [100E3])),
+        ss.PiOver2Pulse(window=ss.BB1),
+        ss.KDD(2E-3, ss.BB1(pi, window=ss.GaussianPulse)),
+        ss.PiOver2Pulse(phase=pi/2, window=ss.BB1)
+        ],
 }
 
 compiled, durations = ss.compile_sequence(seq, False)
