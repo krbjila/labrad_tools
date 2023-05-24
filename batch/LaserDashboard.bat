c671b5e38be05424375d3f639b2c90a7853ba770
KRb JILA <krb.jila@gmail.com>
2023-05-24 16:22:21 -0600

    scripts for starting labrad on polarkrb computer

---
+++ batch/LaserDashboard.bat
@@ -0,0 +1,2 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+start C:\ProgramData\Anaconda3\pythonw.exe C:\Users\krbji\Desktop\labrad_tools\log\dashboard.py
---
+++ batch/StartLabrad.bat
@@ -0,0 +1,2 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+start C:\ProgramData\Anaconda3\python.exe -m labrad.node
