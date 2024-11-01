8fcacef79ca9912027b642bc1eefcc95a466ec05
KRb JILA <krb.jila@gmail.com>
2024-10-31 18:12:23 -0600

    dg4000 server, parameter

---
+++ awgs/RigolDG4000Server.py
@@ -0,0 +1,254 @@
+"""
+Provides access to Rigol DG800 series AWGs.
+
+..
+    ### BEGIN NODE INFO
+    [info]
+    name = dg4000
+    version = 1
+    description = server for Rigol DG800 series AWGs
+    instancename = %LABRADNODE%_dg4000
+
+    [startup]
+    cmdline = %PYTHON% %FILE%
+    timeout = 20
+
+    [shutdown]
+    message = 987654321
+    timeout = 20
+    ### END NODE INFO
+"""
+
+import sys
+from labrad.util import getNodeName
+from labrad.server import LabradServer, setting
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+
+class DG4000Server(LabradServer):
+    """Provides access to Rigol DG800 series AWGs."""
+
+    name = "%LABRADNODE%_dg4000"
+
+    def __init__(self):
+        LabradServer.__init__(self)
+
+    @inlineCallbacks
+    def initServer(self):
+        """
+        initServer(self)
+
+        Called by LabRAD when server is started. Connects to :class:`usb.USB_server`.
+        """
+        self.USB_server_name = "{}_usb".format(getNodeName())
+        self.USB = yield self.client.servers[self.USB_server_name]
+
+    @setting(5, returns="*s")
+    def get_devices(self, c):
+        """
+        get_devices(self, c)
+
+        Lists connected DG4000 AWGs. Note that the function connects to each device to check its ID.
+
+        Args:
+            c: A LabRAD context (which is passed on to passed to :meth:`select_device`)
+
+        Yields:
+            A list of strings corresponding to the IDs of connected DG4000 AWGs
+        """
+        interfaces = yield self.USB.get_interface_list()
+        self.devices = []
+        for i in interfaces:
+            self.select_device(c, i)
+            try:
+                id = yield self.USB.query("*IDN?")
+                if "DG4202" in id:
+                    self.devices.append(i)
+            except Exception as e:
+                print("Could not connect to {}".format(i))
+                print(e)
+        returnValue(self.devices)
+
+    @setting(6, device="s")
+    def select_device(self, c, device):
+        """
+        select_device(self, c, device)
+
+        Select a connected DG4000 AWG
+
+        Args:
+            c: A LabRAD context (not used)
+            device (string): The ID of the DG4000 AWG to connect to, as returned by :meth:`get_devices`
+        """
+        self.USB.select_interface(device)
+
+    @setting(7, channel="i", freq="v", amplitude="v", offset="v", phase="v")
+    def set_sin(self, c, channel, freq, amplitude, offset, phase):
+        """
+        set_sin(self, c, channel, freq, amplitude, offset, phase)
+
+        Get the settings for a channel with sine output
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to get settings for. Must be 1 or 2
+            freq (float): The frequency in Hertz (1E-6 to 200E6)
+            amplitude (float): The amplitude in Volts (limited by impedance and amplitude high level settings)
+            offset (float): The offset in Volts (limited by impedance and amplitude high level settings)
+            phase (float): The phase in degrees (0 to 360)
+
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        yield self.USB.write(
+            ":SOUR%d:APPL:SIN %f,%f,%f,%f"
+            % (channel, min(200e6, max(1e-6, freq)), amplitude, offset, phase)
+        )
+
+    @setting(8, channel="i", enable="b")
+    def set_output(self, c, channel, enable):
+        """
+        set_output(self, c, channel, enable)
+
+        Enables or disables one of the AWG's outputs
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to set the output enable status. Must be 1 or 2
+            enable (bool): True if the channel is enabled, False otherwise
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        if enable:
+            stringthing = "ON"
+        else:
+            stringthing = "OFF"
+        yield self.USB.write(":OUTP%d %s" % (channel, stringthing))
+
+    @setting(11, channel="i", impedance="i", inf="b", low="b")
+    def set_impedance(self, c, channel, impedance=50, inf=False, low=False):
+        """
+        set_impedance(self, c, channel, impedance=50, inf=False, low=False)
+
+        Set the output impedance of a channel either to
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to set the output impedance for. Must be 1 or 2
+            impedance (numeric, optional): Output impedance in Ohms. Defaults to 50, is coerced to be between 1 and 10000.
+            inf (bool, optional): Sets the output to high imedance. Takes precedence over the impedance option and low impedance mode. Defaults to False.
+            low (bool, optional): Sets the output to minimum impedance. Takes precedence over the impedance option. Defaults to False.
+        """
+
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        if inf:
+            yield self.USB.write(":OUTP%d:IMP INF" % (channel))
+
+        elif low:
+            yield self.USB.write(":OUTP%d:IMP MIN" % (channel))
+
+        else:
+            yield self.USB.write(
+                ":OUTP%d:IMP %d" % (channel, min(10000, max(1, impedance)))
+            )
+
+    @setting(12, channel="i", ncycles="i")
+    def set_ncycles(self, c, channel, ncycles):
+        """
+        set_ncycles(self, c, channel, ncycles)
+
+        Sets the number of cycles to run per trigger, if the mode is set to triggered by :meth:`set_gated`.
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to set the number of cycles for. Must be 1 or 2
+            ncycles (int): The number of cycles to run per trigger. Coerced to between 1 and 500000
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        yield self.USB.write(
+            ":SOUR%d:BURS:NCYC %d" % (channel, min(500000, max(1, ncycles)))
+        )
+
+    @setting(13, channel="i", gated="b")
+    def set_gated(self, c, channel, gated):
+        """
+        set_gated(self, c, channel, gated)
+
+        Sets a channel to either be gated or triggered by the external output
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to set the gating mode for. Must be 1 or 2
+            gated (bool): Whether the channel is gated (True) or triggered (False)
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        if gated:
+            yield self.USB.write(":SOUR%d:BURS:MODE GAT" % (channel))
+        else:
+            yield self.USB.write(":SOUR%d:BURS:MODE TRIG" % (channel))
+
+    @setting(9, channel="i", returns="*v")
+    def get_sin(self, c, channel):
+        """
+        get_sin(self, c, channel)
+
+        Get the settings for a channel with sine output
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to get settings for. Must be 1 or 2
+
+        Returns:
+            A list of floats: The frequency (Hz), amplitude (V), offset (V) and phase (deg) of the channel's sine wave. If the channel is not in sine mode, returns :code:`[0]`.
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        out = yield self.USB.query(":SOUR%d:APPL?" % (channel))
+        splits = out.replace('"', "").split(",")
+        if "SIN" in splits[0]:
+            returnValue([float(i) for i in splits[1:]])
+        else:
+            returnValue([0])
+
+    @setting(10, channel="i", returns="b")
+    def get_output(self, c, channel):
+        """
+        get_output(self, c, channel)
+
+        Check whether an output is enabled
+
+        Args:
+            c: A LabRAD context (not used)
+            channel (int): Which channel to get output status. Must be 1 or 2
+
+        Returns:
+            boolean: whether the channel's output is enabled
+        """
+        if channel not in [1, 2]:
+            raise ValueError(
+                "Channel {} invalid. Acceptable values are 1 or 2.".format(channel)
+            )
+        stringthing = yield self.USB.query(":OUTP%d?" % (channel))
+        out = "ON" in stringthing
+        returnValue(out)
+
+
+if __name__ == "__main__":
+    from labrad import util
+
+    util.runServer(DG4000Server())
---
+++ conductor/devices/dg4000/sin.py
@@ -0,0 +1,97 @@
+import sys
+
+sys.path.append("../")
+from generic_device.generic_parameter import GenericParameter
+
+from twisted.internet.defer import inlineCallbacks, Deferred
+from twisted.internet.reactor import callLater
+from labrad.wrappers import connectAsync
+
+from conductor_device.conductor_parameter import ConductorParameter
+
+
+class Sin(ConductorParameter):
+    """
+    Sin(ConductorParameter)
+
+    Conductor parameter for controlling the frequency, amplitude (Vpp), offset (V), phase (deg), and enable and gating status of each of the Rigol DG800's channels. Example config:
+
+    .. code-block:: json
+
+            {
+                "dg4000": {
+                    "sin": [{
+                        "freq2": 100,
+                        "amplitude2": 0.31,
+                        "phase2": 0,
+                        "offset2": 0,
+                        "output2": 0,
+                    }]
+                }
+            }
+    """
+
+    priority = 3
+
+    def __init__(self, config={}):
+        super(Sin, self).__init__(config)
+        self.value = [self.default_sin]
+
+    @inlineCallbacks
+    def initialize(self):
+        self.cxn = yield connectAsync()
+        devices = yield self.cxn.polarkrb_dg4000.get_devices()
+        try:
+            yield self.cxn.polarkrb_dg4000.select_device(devices[0])
+            # yield self.cxn.polarkrb_dg4000.set_impedance(1, 50)
+            yield self.cxn.polarkrb_dg4000.set_impedance(2, 50)
+
+            # yield self.cxn.polarkrb_dg4000.set_sin(
+            #     1,
+            #     self.value["freq1"],
+            #     self.value["amplitude1"],
+            #     self.value["offset1"],
+            #     self.value["phase1"],
+            # )
+            yield self.cxn.polarkrb_dg4000.set_sin(
+                2,
+                self.value["freq2"],
+                self.value["amplitude2"],
+                self.value["offset2"],
+                self.value["phase2"],
+            )
+            # yield self.cxn.polarkrb_dg4000.set_output(1, bool(self.value["output1"]))
+            yield self.cxn.polarkrb_dg4000.set_output(2, bool(self.value["output2"]))
+            # yield self.cxn.imaging_dg800.set_ncycles(1,int(self.value['ncycles1']))
+            # yield self.cxn.imaging_dg800.set_ncycles(2,int(self.value['ncycles2']))
+            # yield self.cxn.imaging_dg800.set_gated(1,bool(self.value['gated1']))
+            # yield self.cxn.imaging_dg800.set_gated(2,bool(self.value['gated2']))
+        except Exception as e:
+            print(e)
+
+    @inlineCallbacks
+    def update(self):
+        if self.value:
+            try:
+                # yield self.cxn.imaging_dg800.set_sin(
+                #     1,
+                #     self.value["freq1"],
+                #     self.value["amplitude1"],
+                #     self.value["offset1"],
+                #     self.value["phase1"],
+                # )
+                yield self.cxn.imaging_dg800.set_sin(
+                    2,
+                    self.value["freq2"],
+                    self.value["amplitude2"],
+                    self.value["offset2"],
+                    self.value["phase2"],
+                )
+                # yield self.cxn.imaging_dg800.set_output(1, bool(self.value["output1"]))
+                yield self.cxn.imaging_dg800.set_output(2, bool(self.value["output2"]))
+                # yield self.cxn.imaging_dg800.set_ncycles(1,int(self.value['ncycles1']))
+                # yield self.cxn.imaging_dg800.set_ncycles(2,int(self.value['ncycles2']))
+                # yield self.cxn.imaging_dg800.set_gated(1,bool(self.value['gated1']))
+                # yield self.cxn.imaging_dg800.set_gated(2,bool(self.value['gated2']))
+            except Exception as e:
+                print(e)
